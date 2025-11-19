e77d5f74b398d1e1fc91e5512c016544712d732f
Jannael Orlando <orlandojannael@gmail.com>
2025-11-19 13:38:20 -0600

    newSemester, fn

--- src/backend/function/score/function.ts
+++ src/backend/function/score/function.ts
@@ -4,6 +4,7 @@ import getToken from '../../utils/token'
 import dotenv from 'dotenv'
 import { IEnv } from '../../interface/env'
 import { UserBadRequest } from '../../error/error'
+import { IScore } from '../../interface/score'
 dotenv.config({ quiet: true })
 const {
   CRYPTO_ACCESS_TOKEN_ENV,
@@ -14,10 +15,16 @@ const {
 
 const functions = {
   user: {
-    get: async function (req: Request, res: Response) {
+    get: async function (req: Request, res: Response): Promise<IScore> {
       const accessToken = getToken(req, 'accessToken', JWT_ACCESS_TOKEN_ENV, CRYPTO_ACCESS_TOKEN_ENV)
       if (req.body.semester === undefined) throw new UserBadRequest('Missing data', 'You need to send the semester you want')
       return await model.user.get(accessToken.account, req.body.semester)
+    },
+    newSemester: async function (req: Request, res: Response): Promise<boolean> {
+      const accessToken = getToken(req, 'accessToken', JWT_ACCESS_TOKEN_ENV, CRYPTO_ACCESS_TOKEN_ENV)
+      if (req.body?.subject === undefined || Array.isArray(req.body?.subject)) throw new UserBadRequest('Missing data', 'You need to send the subjects you want to add to the new semester')
+
+      return await model.user.newSemester(accessToken.account, req.body.subject)
     }
   }
 }
