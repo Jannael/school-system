2cc1ce8696aad276a21d29826bc142514577777e
Jannael Orlando <orlandojannael@gmail.com>
2025-11-18 20:57:40 -0600

    score dbModel

---
+++ src/backend/database/schemas/node/score.ts
@@ -0,0 +1,20 @@
+import { model, Schema } from 'mongoose'
+import config from '../../../config/config'
+
+const partialSchema = new Schema({
+  subject: { type: String, required: true },
+  score: { type: Number, required: true }
+})
+
+const schema = new Schema({
+  account: { type: String, required: true, unique: true },
+  semester: { type: String, required: true },
+  One: [partialSchema],
+  Two: [partialSchema],
+  Three: [partialSchema]
+}, {
+  ...config.database.mongodb.schemaOptions,
+  collection: 'score'
+})
+
+export default model('score', schema)
